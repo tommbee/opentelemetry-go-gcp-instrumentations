f4b81c2fbf33b1a5e8db68ba95ae1df26c697ba6
Tom Bennett <tombennett86@hotmail.co.uk>
2022-07-14 22:31:29 +0100

    further unit tests

--- otelpubsub/pubsub_test.go
+++ otelpubsub/pubsub_test.go
@@ -2,12 +2,35 @@ package otelpubsub
 
 import (
 	"cloud.google.com/go/pubsub"
+	"context"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/trace"
+	"log"
 	"testing"
 )
 
 func TestSubscriptionWithTracing(t *testing.T) {
+	tracer := trace.NewNoopTracerProvider()
+	propagators := otel.GetTextMapPropagator()
+	actual := SubscriptionWithTracing(&pubsub.Subscription{}, WithTracerProvider(tracer), WithPropagators(propagators))
+
+	if actual.config.tracerProvider != tracer {
+		t.Error("Trace provider not set")
+	}
+
+	if actual.config.propagators != propagators {
+		t.Error("Propagators not set")
+	}
+}
+
+func TestSubscription_Receive(t *testing.T) {
 	actual := SubscriptionWithTracing(&pubsub.Subscription{})
-	if actual.config == nil {
-		t.Error("Config was not set")
+	err := actual.Receive(context.Background(), func(ctx context.Context, m *pubsub.Message) {
+		log.Printf("Got message: %q\n", string(m.Data))
+		m.Ack()
+	})
+
+	if err != nil {
+		t.Fatal(err)
 	}
 }
